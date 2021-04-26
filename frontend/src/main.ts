7254cc6b5262f1eac19cad122d56a1a335a4c3e4
hanfeng zhang <zhf@33.cn>
2021-04-26 17:33:03 +0800

    123

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import "tailwindcss/tailwind.css"
 
 Vue.config.productionTip = false
 
